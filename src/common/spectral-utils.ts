2a093ea1bdad282663f85f700c51129697d0d308
guylev008 <guy@spectralops.io>
2023-07-10 17:24:37 +0300

    code review fixes

--- src/common/spectral-utils.ts
+++ src/common/spectral-utils.ts
@@ -9,20 +9,14 @@ export const shouldUpdateSpectralAgent = (): boolean => {
   )
 }
 
-const getLastAgentUpdateDate = (): number | undefined => {
-  return PersistenceContext.getInstance().getGlobalStateValue<number>(
-    AGENT_LAST_UPDATE_DATE
-  )
-}
-
 const isOneWeekPassedSinceLastUpdate = (): boolean => {
-  const lastUpdateDate = getLastAgentUpdateDate()
+  const lastUpdateDate =
+    PersistenceContext.getInstance().getGlobalStateValue<number>(
+      AGENT_LAST_UPDATE_DATE
+    )
   if (!lastUpdateDate) {
     return true
   }
   const oneWeekInMs = 7 * 24 * 3600 * 1000
-  if (Date.now() - lastUpdateDate > oneWeekInMs) {
-    return true
-  }
-  return false
+  return Date.now() - lastUpdateDate > oneWeekInMs
 }
